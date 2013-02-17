2b44ce4f66710dee82e0b943b5b7ec3e09a8eaa0
原田 勝信 <harada@90201573N.local>
2013-02-17 13:40:10 +0900

    Initial Commit

---
+++ MyObjCApp2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MyObjCApp2
+//
+//  Created by 原田 勝信 on 2013/02/17.
+//  Copyright (c) 2013年 原田 勝信. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
